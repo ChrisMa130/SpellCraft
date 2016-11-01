3d2a629d769e58ef39b0ce1680be71b6f11760eb
Ting Cheng <chengt4@uw.edu>
2016-11-01 11:54:13 -0700

    cleanup spell class. created duplicate SpellManager (same script as SpellManager_Prototype)

--- Assets/Scripts/Spell/Spell.cs
+++ Assets/Scripts/Spell/Spell.cs
@@ -4,50 +4,22 @@ using System.Collections;
 public class Spell : MonoBehaviour {
 
 	private string spellName = null;
-	private float damage = 1f;
-	private float mpCost = 1f;
-	private GameObject spellEffect;
+	private float damage = 0f;
+	private float mpCost = 0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	/* constructor
-	void newSpell(String spellName, float damage, float mpCost, GameObject spellEffect) {
-		this.spellName = spellName;
-		this.damage = damage;
-		this.mpCost = mpCost;
-		this.spellEffect = spellEffect;
-	}*/
-
+	// get damage of spell
 	public float getDamage() {
 		return this.damage;
 	}
 
+	// get damage of spell
+	public float getMPCost() {
+		return this.mpCost;
+	}
+
 	// reduce Player health on collision, and destroy spell instance
-	void OnCollisionEnter(Collision collision) {
+	void OnTriggerEnter(Collider collision) {
 		// reduce player health
 		collision.gameObject.GetComponent<Player>().ReduceHealth(getDamage());
-		//Destroy (gameObject); Not needed, will get rid of explosion animation.
-	}
-
-	// destroy spell after hitting room boundary
-	void OnTriggerEnter(Collider collision) {
-		
-	}
-
-	// display spell Effect
-	void displayEffect(){
-		Vector3 offset = Camera.main.transform.forward * 0.5f;
-		if (spellEffect != null) {
-			GameObject spell = Instantiate(spellEffect, Camera.main.transform.position + offset, Camera.main.transform.rotation) as GameObject;
-			spell.GetComponent<EffectSettings> ().Target = GameObject.Find ("BasicCursor");
-		}
 	}
 }
