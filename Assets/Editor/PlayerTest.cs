c9210a5f729dd8febc761e2601fae51cf3220fbd
mstaehel <mstaehel@cs.washington.edu>
2016-11-02 09:23:05 -0700

    Player test suite added to part 2

--- Assets/Editor/PlayerTest.cs
+++ Assets/Editor/PlayerTest.cs
@@ -38,6 +38,17 @@ public class PlayerTest {
     }
 
     [Test]
+    public void TestNormalDamage()
+    {
+        PlayerTestClass player = new PlayerTestClass();
+        int health = player.getHealth();
+        Assert.AreEqual(health, 100);
+        health = player.modifyHealth(20);
+        Assert.AreEqual(health, 80);
+        Assert.AreEqual(player.getHealth(), 80);
+    }
+
+    [Test]
     public void TestMagicCanNotGoBelowZero()
     {
         PlayerTestClass player = new PlayerTestClass();
@@ -46,8 +57,19 @@ public class PlayerTest {
         magic = player.modifyMagic(10);
         Assert.AreEqual(magic, 0);
         Assert.AreEqual(player.getMagic(), 0);
-        magic = player.modifyMagic(-2);
+        magic = player.modifyMagic(2);
         Assert.AreEqual(magic, -1);
         Assert.AreEqual(player.getMagic(), 0);
+    }
+
+    [Test]
+    public void TestMagicCanNotGoOverTen()
+    {
+        PlayerTestClass player = new PlayerTestClass();
+        int magic = player.getMagic();
+        Assert.AreEqual(magic, 10);
+        magic = player.modifyMagic(-1);
+        Assert.AreEqual(magic, 10);
+        Assert.AreEqual(player.getMagic(), 10);
     }
 }
