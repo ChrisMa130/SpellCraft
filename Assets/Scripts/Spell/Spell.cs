f089dcfced7cc04b84b4343e67f97e4b666fe6dd
mstaehel <mstaehel@cs.washington.edu>
2016-11-05 13:52:28 -0700

    Fixing spell error. Changed type to int for magic and damage, altered 0f to 0 so that compiler less likely to complain.

--- Assets/Scripts/Spell/Spell.cs
+++ Assets/Scripts/Spell/Spell.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class Spell : MonoBehaviour {
 
 	private string spellName = null;
-	private float damage = 0f;
-	private float mpCost = 0f;
+	private float damage = 0;
+	private float mpCost = 0;
 
 	// get damage of spell
 	public float getDamage() {
