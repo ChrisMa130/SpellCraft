4370055b5436bbb1f2e0c43eb51548e4d7edf6da
Leonardo Aoun <Leonardo Aoun>
2016-11-08 11:27:36 -0800

    cursor is changing colors

---
+++ Assets/Scripts/UI_Scripts/ButtonManager_Battle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonManager_Battle : ButtonManager_Menu {
+
+	// Use this for initialization
+	void Start () {
+
+		base.Start ();
+		foreach (Button btn in components)
+		{
+
+			switch (btn.name)
+			{
+			case "Back": btn.onClick.AddListener(onBtnBackClick);  break;
+
+			default: break;
+			}
+		}
+	}
+	
+
+}
