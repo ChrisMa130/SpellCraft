d586e4b3752bbf4fd046627800a51ac59db8861d
Leonardo Aoun <Leonardo Aoun>
2016-11-08 18:52:57 -0800

    figured out how to represent mana

--- Assets/Scripts/UI_Scripts/Game_UIManager.cs
+++ Assets/Scripts/UI_Scripts/Game_UIManager.cs
@@ -1,20 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Game_UIManager : MonoBehaviour {
 
 	GameObject cursor;
 	GameObject mainCam;
+	HoloToolkit.Unity.BasicCursor basicCursor;
 	Player player;
 	Material cursorMat;
+	Mesh[] manaBalls;
+	Transform manaBall_1;
+
 
 
 
 	void Start(){
 		cursor = GameObject.FindWithTag ("Cursor");
+		basicCursor = cursor.GetComponent<HoloToolkit.Unity.BasicCursor> ();
 		cursorMat = cursor.GetComponent<Renderer> ().material;
+
 		mainCam = GameObject.FindWithTag ("MainCamera");
 		player = Player.Instance;
+		//manaBalls = cursor.GetComponentsInChildren<Mesh>();
+
+		//Debug.Log ("mana Balls: " + manaBalls.Length);
+		manaBall_1 = cursor.gameObject.transform.GetChild(0);
+
+		Renderer test = manaBall_1.GetComponent<Renderer> ();
+
+		if (test == null) {
+			Debug.Log ("test ball 1 is null");
+		} else {
+			Debug.Log ("test bull 1 exists");
+		}
+
+		Color c = test.material.color;
+
+		c.r = 1f;
+		c.g = 0f;
+		c.b = 0f;
+
+		test.material.color = c;
+
+
+
 	}
 
 	void Awake()
@@ -38,17 +68,10 @@ public class Game_UIManager : MonoBehaviour {
 
 
 	public void updateCursorColor(){
+		float p = player.getHealthPercentage();
+		basicCursor.updateColor(p);
 
-		if (cursorMat != null) {
-			float p = player.getHealthPercentage();
-			Color c = cursorMat.color;
-			c.r = 1 - p;
-			c.g = p;
-			c.b = p/2;
-			cursorMat.color = c;
-
-
-		}
 	}
+
 
 }
